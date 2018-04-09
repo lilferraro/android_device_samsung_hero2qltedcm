eff6c54b5625258f7848c7223a5df6f90b2c93be
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-04-09 23:17:47 +0900

    import thermal hal from LA.UM.6.5.r1-06700-8x96.0
    sources: https://source.codeaurora.org/quic/la/platform/hardware/qcom/thermal/tree/?h=LA.UM.6.5.r1-06700-8x96.0

--- device.mk
+++ device.mk
@@ -452,6 +452,13 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8996
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
